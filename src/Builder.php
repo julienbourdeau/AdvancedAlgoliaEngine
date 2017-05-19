65db1a36130069cadd5653329117e2d605f5a25a
Julien Bourdeau <julien@sigerr.org>
2017-05-19 15:28:54 +0000

    Add raw() capability

--- src/Builder.php
+++ src/Builder.php
@@ -7,5 +7,13 @@ use Laravel\Scout\Builder as ScoutBuilder;
 
 class Builder extends ScoutBuilder
 {
-
+    /**
+     * Return the raw result from the engine.
+     *
+     * @return mixed
+     */
+    public function raw()
+    {
+        return $this->engine()->search($this);
+    }
 }
